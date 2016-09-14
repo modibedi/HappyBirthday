58f58ea1c75699734a467b2996745e04334e83eb
Herry <hdmodibedi@gmail.com>
2016-09-14 15:41:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ekasi/happybirthday/HappyBirthdayCard.java
@@ -0,0 +1,13 @@
+package com.example.ekasi.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HappyBirthdayCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_birthday_card);
+    }
+}
